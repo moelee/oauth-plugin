4f384fdf91ff2f5122617baa70809ca66e74000d
jack0 <jack@fooala.com>
2009-04-06 17:02:15 -0400

    Added two more tables to migration.rb: table_resources and child_sps. Also added additional information to client_applications and oauth_tokens

--- generators/oauth_provider/templates/migration.rb
+++ generators/oauth_provider/templates/migration.rb
@@ -8,6 +8,10 @@ class CreateOauthTables < ActiveRecord::Migration
       t.string :key, :limit => 50
       t.string :secret, :limit => 50
       t.integer :user_id
+      #start_additions
+      t.string :resource_scope
+      t.string :public_key
+      #end_additions
 
       t.timestamps
     end
@@ -19,10 +23,13 @@ class CreateOauthTables < ActiveRecord::Migration
       t.integer :client_application_id
       t.string :token, :limit => 50
       t.string :secret, :limit => 50
+      #start_additions
+      t.timestamp :expired_on
+      #end_additions
       t.timestamp :authorized_at, :invalidated_at
+
       t.timestamps
     end
-    
     add_index :oauth_tokens, :token, :unique
     
     create_table :oauth_nonces do |t|
@@ -32,6 +39,25 @@ class CreateOauthTables < ActiveRecord::Migration
       t.timestamps
     end
     add_index :oauth_nonces,[:nonce, :timestamp], :unique
+
+    #start_addition
+    create_table :child_sps do |t|
+      t.string :base_url
+      t.string :shared_secret
+      t.string :resources
+
+      t.timestamps
+    end
+    add_index :child_sps, :unique
+
+    create_table :table_resources do |t|
+      t.string :name
+      t.integer :child_sp_id
+
+      t.timestamps
+    end
+    add_index :table_resources, :name, :unique
+    #end_addition
     
   end
 
@@ -39,6 +65,10 @@ class CreateOauthTables < ActiveRecord::Migration
     drop_table :client_applications
     drop_table :oauth_tokens
     drop_table :oauth_nonces
+    #start_addition
+    drop_table :child_sps
+    drop_table :table_resources
+    #end_addition
   end
 
 end
